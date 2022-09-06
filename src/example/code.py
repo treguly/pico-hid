a7b53fd1dc8977d54d785043eec415965f6f2716
Tyler Reguly <ht@computerdefense.org>
2022-09-06 19:59:04 -0400

    Updated Example code.py to match code.py

--- src/example/code.py
+++ src/example/code.py
@@ -1,9 +1,17 @@
+# type: ignore
+# code.py template for Raspberry Pi Pico 
+# Tested using Circuit Python 7.0.0
+# Requires adafruit_hid library
 
 import usb_hid
 import time
 import usb_hid_map as usb
 
 from adafruit_hid.keyboard import Keyboard
+
+# Disable autoreload
+import supervisor
+supervisor.disable_autoreload()
 
 kbd = Keyboard(usb_hid.devices)
 
